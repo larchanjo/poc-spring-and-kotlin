d3c626e165e3070e349b5f581199d9b96b83dd94
Luram Archanjo <luram.archanjo@sensedia.com>
2019-01-30 08:48:43 -0200

    first commit

--- src/main/kotlin/com/sensedia/kotlin/domain/OpenWeatherApi.kt
+++ src/main/kotlin/com/sensedia/kotlin/domain/OpenWeatherApi.kt
@@ -7,6 +7,6 @@ class OpenWeatherApi(val apiKey: String, val restTemplate: RestTemplate) {
     // List of cities http://bulk.openweathermap.org/sample/city.list.json.gz
     val cityName = "Estado de São Paulo"
     val countryCode = "BR"
-    val forecastUri = "http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY}&appid={APIKEY}";
+    val weatherUri = "http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY}&appid={APIKEY}";
 
 }
