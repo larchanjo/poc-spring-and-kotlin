c3400cf07eb2e620f14ba685347c0a4ab573f476
Luram Archanjo <luram.archanjo@sensedia.com>
2019-01-30 10:15:48 -0200

    Weather

--- src/main/kotlin/com/sensedia/kotlin/domain/OpenWeatherApi.kt
+++ src/main/kotlin/com/sensedia/kotlin/domain/OpenWeatherApi.kt
@@ -1,6 +1,6 @@
 package com.sensedia.kotlin.domain
 
-import com.sensedia.kotlin.domain.model.WeatherResponse
+import com.sensedia.kotlin.domain.model.GetWeatherResponse
 import org.slf4j.LoggerFactory
 import org.springframework.web.client.RestTemplate
 
@@ -8,11 +8,11 @@ class OpenWeatherApi(val apiKey: String, val restTemplate: RestTemplate) {
 
     val log = LoggerFactory.getLogger(this.javaClass)
 
-    fun getWeather(cityName: String, countryCode: String): WeatherResponse? {
+    fun getWeather(cityName: String, countryCode: String): GetWeatherResponse? {
         log.info("Getting weather form $cityName and $countryCode")
         val weatherUri = "http://api.openweathermap.org/data/2.5/weather?q=$cityName,$countryCode&appid=$apiKey"
         log.info("Requesting $weatherUri")
-        return restTemplate.getForObject(weatherUri, WeatherResponse::class.java)
+        return restTemplate.getForObject(weatherUri, GetWeatherResponse::class.java)
     }
 
 }
--- src/main/kotlin/com/sensedia/kotlin/domain/model/GetWeather.kt
+++ src/main/kotlin/com/sensedia/kotlin/domain/model/GetWeather.kt
@@ -1,5 +1,5 @@
 package com.sensedia.kotlin.domain.model
 
-data class WeatherResponse(val weather: List<Weather> = listOf())
+data class GetWeatherResponse(val weather: List<Weather> = listOf())
 
 data class Weather(val id: String, val main: String)
